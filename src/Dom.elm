7e9e4363e228d01b3e109120d03d002a979d8d14
Daniel C Narey <danielnarey@users.noreply.github.com>
2018-09-17 23:44:29 -0400

    styles, actions, input handlers

--- src/Dom.elm
+++ src/Dom.elm
@@ -1,30 +1,18 @@
 module Dom exposing
   ( Element, element
   , setId
-  , addClass, addClassConditional, addClassList, addClassListConditional
+  , addClass, addClassConditional
+  , addClassList, addClassListConditional
   , removeClass, replaceClassList
+  , addStyle, addStyleConditional
+  , addStyleList, addStyleListConditional
+  , removeStyle, replaceStyleList
   , getInternal
   )
 
 
 import VirtualDom
-
-
-{-| Specification of the internal record type. This is not exposed.
-
--}
-type alias Internal msg =
-  { tag : String
-  , id : String
-  , classes : List String
-  , styles : List (String, String)
-  , actions: List (String, msg)
-  , attributes : List (VirtualDom.Attribute msg)
-  , text : String
-  , children : List (VirtualDom.Node msg)
-  , namespace : String
-  , keys : List String
-  }
+import Json.Decode
 
 
 {-| A record containing all of the data needed to construct an HTML node (via
@@ -37,6 +25,33 @@ passed as an argument to the `render` function.
 -}
 type Element msg =
   Element (Internal msg)
+
+
+{-| Specification of the internal record type. This is not exposed.
+
+-}
+type alias Internal msg =
+  { tag : String
+  , id : String
+  , classes : List String
+  , styles : List (String, String)
+  , actions: List (String, Handler msg)
+  , attributes : List (VirtualDom.Attribute msg)
+  , text : String
+  , children : List (VirtualDom.Node msg)
+  , namespace : String
+  , keys : List String
+  }
+
+
+{-| Options for custom event handlers in VirtualDom
+
+-}
+type alias Handler msg =
+  { message : msg
+  , stopPropagation : Bool
+  , preventDefault : Bool
+  }
 
 
 -- CONSTRUCTOR
@@ -128,8 +143,8 @@ addClassListConditional (ls, test) =
     False -> identity
 
 
-{-| Delete all instances of a class name or from the current list contained in
-an `Element` record
+{-| Delete all instances of a class name from the current list contained in an
+`Element` record
 
 -}
 removeClass : String -> Element msg -> Element msg
@@ -138,7 +153,7 @@ removeClass s =
 
 
 {-| Delete the current list of class names contained in current `Element`
-record, replacing it a new list of class names
+record, replacing it with a new list of class names
 
 -}
 replaceClassList : List String -> Element msg -> Element msg
@@ -166,27 +181,206 @@ addStyleConditional (kv, test) =
   case test of
     True -> addStyle kv
     False -> identity
-    
---
--- -- THIS IS WHERE I LEFT OFF...
---
--- addStyleList : List (String, String) -> Element msg -> Element msg
--- addStyleListConditional : (List (String, String), Bool) -> Element msg -> Element msg
--- removeStyle : String -> Element msg -> Element msg
--- replaceStyleList : List (String, String) -> Element msg -> Element msg
---
--- -- event listeners that don't capture an input value
--- addAction : (String, msg) -> Element msg -> Element msg
--- addActionConditional : ((String, msg), Bool) -> Element msg -> Element msg
--- addActionStopPropagation : (String, msg) -> Element msg -> Element msg
--- addActionPreventDefault : (String, msg) -> Element msg -> Element msg
--- addActionStopAndPrevent : (String, msg) -> Element msg -> Element msg
--- removeAction : String -> Element msg -> Element msg
---
--- -- event listeners that capture an input value (immediately rendered to `VirtualDom.Attribute`)
--- setInputHandler : (String -> msg) -> Element msg -> Element msg
--- setInputHandlerWithParser : (a -> msg, String -> a) -> Element msg -> Element msg
--- setCheckHandler : (Bool -> msg) -> Element msg -> Element msg
+
+
+{-| Add a list of style key/value pairs to the current list contained in an
+`Element` record
+
+-}
+addStyleList : List (String, String) -> Element msg -> Element msg
+addStyleList lkv =
+  modify (\n -> { n | styles = List.append n.styles lkv })
+
+
+{-| Add a list of style key/value pairs to the current list contained in an
+`Element` record when the boolean argument evaluates to `True`
+
+-}
+addStyleListConditional : (List (String, String), Bool) -> Element msg -> Element msg
+addStyleListConditional (lkv, test) =
+  case test of
+    True -> addStyleList lkv
+    False -> identity
+
+
+{-| Delete all instances of a style key from the current list contained in an
+`Element` record
+
+-}
+removeStyle : String -> Element msg -> Element msg
+removeStyle s =
+  let
+    isNotKey arg (k, v) =
+      k /= arg
+
+  in
+    modify (\n -> { n | styles = n.styles |> List.filter (isNotKey s) })
+
+
+{-| Delete the current list of style key/value pairs contained in current
+`Element` record, replacing it with a new list of style key/value pairs
+
+-}
+replaceStyleList : List (String, String) -> Element msg -> Element msg
+replaceStyleList lkv =
+  modify (\n -> { n | styles = lkv })
+
+
+-- ACTION
+---- event listeners that don't capture an input value
+
+addAction : (String, msg) -> Element msg -> Element msg
+addAction (k, v) =
+  let
+    newAction =
+      (k, Handler v False False)
+
+  in
+    modify (\n -> { n | actions = List.append n.actions [ newAction ] })
+
+
+addActionConditional : ((String, msg), Bool) -> Element msg -> Element msg
+addActionConditional (kv, test) =
+  case test of
+    True -> addAction kv
+    False -> identity
+
+
+addActionStopPropagation : (String, msg) -> Element msg -> Element msg
+addActionStopPropagation (k, v) =
+  let
+    newAction =
+      (k, Handler v True False)
+
+  in
+    modify (\n -> { n | actions = List.append n.actions [ newAction ] })
+
+
+addActionPreventDefault : (String, msg) -> Element msg -> Element msg
+addActionPreventDefault (k, v) =
+  let
+    newAction =
+      (k, Handler v False True)
+
+  in
+    modify (\n -> { n | actions = List.append n.actions [ newAction ] })
+
+
+addActionStopAndPrevent : (String, msg) -> Element msg -> Element msg
+addActionStopAndPrevent (k, v) =
+  let
+    newAction =
+      (k, Handler v True True)
+
+  in
+    modify (\n -> { n | actions = List.append n.actions [ newAction ] })
+
+
+removeAction : String -> Element msg -> Element msg
+removeAction s =
+  let
+    isNotKey arg (k, v) =
+      k /= arg
+
+  in
+    modify (\n -> { n | actions = n.actions |> List.filter (isNotKey s) })
+
+
+-- event listeners that capture an input value (immediately rendered to `VirtualDom.Attribute`)
+
+
+captureValue : (String, String -> Handler msg) -> VirtualDom.Attribute msg
+captureValue (event, receiver) =
+  let
+    handler =
+      Json.Decode.string
+        |> Json.Decode.at [ "target", "value" ]
+        |> Json.Decode.map receiver
+        |> VirtualDom.Custom
+
+  in
+    VirtualDom.on event handler
+
+
+captureChecked : (String, Bool -> Handler msg) -> VirtualDom.Attribute msg
+captureChecked (event, receiver) =
+  let
+    handler =
+      Json.Decode.bool
+        |> Json.Decode.at [ "target", "checked" ]
+        |> Json.Decode.map receiver
+        |> VirtualDom.Custom
+
+  in
+    VirtualDom.on event handler
+
+
+setInputHandler : (String -> msg) -> Element msg -> Element msg
+setInputHandler token =
+  let
+    receiver string =
+      Handler (token string) True False
+
+    newAttribute =
+      captureValue ("input", receiver)
+
+  in
+    modify (\n -> { n | attributes = List.append n.attributes [ newAttribute ] })
+
+
+setInputHandlerWithParser : (a -> msg, String -> a) -> Element msg -> Element msg
+setInputHandlerWithParser (token, parser) =
+  let
+    receiver string =
+      Handler (string |> parser |> token) True False
+
+    newAttribute =
+      captureValue ("input", receiver)
+
+  in
+    modify (\n -> { n | attributes = List.append n.attributes [ newAttribute ] })
+
+
+setChangeHandler : (String -> msg) -> Element msg -> Element msg
+setChangeHandler token =
+  let
+    receiver string =
+      Handler (token string) False False
+
+    newAttribute =
+      captureValue ("change", receiver)
+
+  in
+    modify (\n -> { n | attributes = List.append n.attributes [ newAttribute ] })
+
+
+setChangeHandlerWithParser : (a -> msg, String -> a) -> Element msg -> Element msg
+setChangeHandlerWithParser (token, parser) =
+  let
+    receiver string =
+      Handler (string |> parser |> token) False False
+
+    newAttribute =
+      captureValue ("change", receiver)
+
+  in
+    modify (\n -> { n | attributes = List.append n.attributes [ newAttribute ] })
+
+
+setCheckHandler : (Bool -> msg) -> Element msg -> Element msg
+setCheckHandler token =
+  let
+    receiver bool =
+      Handler (token bool) False False
+
+    newAttribute =
+      captureChecked ("change", receiver)
+
+  in
+    modify (\n -> { n | attributes = List.append n.attributes [ newAttribute ] })
+
+
+
 -- setCustomHandler : (String, Decoder msg) -> Element msg -> Element msg
 -- setCustomHandlerStopPropagation : (String, Decoder msg) -> Element msg -> Element msg
 -- setCustomHandlerPreventDefault : (String, Decoder msg) -> Element msg -> Element msg
--- tests/Tests.elm
+++ tests/Tests.elm
@@ -69,25 +69,28 @@ modifierTests =
 
   , ( \() ->
       Dom.element "div"
-        |> Dom.addClassConditional (testString1, True)
+        |> Dom.addClass testString1
+        |> Dom.addClassConditional (testString2, True)
         |> Dom.getInternal
-        |> Expect.equal { div | classes = [ testString1 ] }
+        |> Expect.equal { div | classes = [ testString1, testString2 ] }
     )
       |> test "`Dom.addClassConditional` result should match a normal record update"
 
   , ( \() ->
       Dom.element "div"
-        |> Dom.addClassConditional (testString1, False)
+        |> Dom.addClass testString1
+        |> Dom.addClassConditional (testString2, False)
         |> Dom.getInternal
-        |> Expect.equal div
+        |> Expect.equal { div | classes = [ testString1 ] }
     )
       |> test "`Dom.addClassConditional` result should match the unmodified test record"
 
   , ( \() ->
       Dom.element "div"
-        |> Dom.addClassList [ testString1, testString2 ]
+        |> Dom.addClass testString1
+        |> Dom.addClassList [ testString2, testString3 ]
         |> Dom.getInternal
-        |> Expect.equal { div | classes = [ testString1, testString2 ] }
+        |> Expect.equal { div | classes = [ testString1, testString2, testString3 ] }
     )
       |> test "`Dom.addClassList` result should match a normal record update"
 
@@ -124,6 +127,74 @@ modifierTests =
         |> Expect.equal { div | classes = [ testString3, testString4 ] }
     )
       |> test "`Dom.replaceClassList` result should match a normal record update"
+
+  , ( \() ->
+      Dom.element "div"
+        |> Dom.addStyle (testString1, testString2)
+        |> Dom.getInternal
+        |> Expect.equal { div | styles = [ (testString1, testString2) ] }
+    )
+      |> test "`Dom.addstyle` result should match a normal record update"
+
+  , ( \() ->
+      Dom.element "div"
+        |> Dom.addStyle (testString1, testString2)
+        |> Dom.addStyleConditional ((testString3, testString4), True)
+        |> Dom.getInternal
+        |> Expect.equal { div | styles = [ (testString1, testString2), (testString3, testString4) ] }
+    )
+      |> test "`Dom.addStyleConditional` result should match a normal record update"
+
+  , ( \() ->
+      Dom.element "div"
+        |> Dom.addStyle (testString1, testString2)
+        |> Dom.addStyleConditional ((testString3, testString4), False)
+        |> Dom.getInternal
+        |> Expect.equal { div | styles = [ (testString1, testString2) ] }
+    )
+      |> test "`Dom.addStyleConditional` result should match the unmodified test record"
+
+  , ( \() ->
+      Dom.element "div"
+        |> Dom.addStyleList [ (testString1, testString2), (testString3, testString4) ]
+        |> Dom.getInternal
+        |> Expect.equal { div | styles = [ (testString1, testString2), (testString3, testString4) ] }
+    )
+      |> test "`Dom.addStyleList` result should match a normal record update"
+
+  , ( \() ->
+      Dom.element "div"
+        |> Dom.addStyleListConditional ([ (testString1, testString2), (testString3, testString4) ], True)
+        |> Dom.getInternal
+        |> Expect.equal { div | styles = [ (testString1, testString2), (testString3, testString4) ] }
+    )
+      |> test "`Dom.addStyleListConditional` result should match a normal record update"
+
+  , ( \() ->
+      Dom.element "div"
+        |> Dom.addStyleListConditional ([ (testString1, testString2), (testString3, testString4) ], False)
+        |> Dom.getInternal
+        |> Expect.equal div
+    )
+      |> test "`Dom.addStyleListConditional` result should match the unmodified test record"
+
+  , ( \() ->
+      Dom.element "div"
+        |> Dom.addStyleList [ (testString1, testString2), (testString3, testString4), (testString1, testString4) ]
+        |> Dom.removeStyle testString1
+        |> Dom.getInternal
+        |> Expect.equal { div | styles = [ (testString3, testString4) ] }
+    )
+      |> test "`Dom.removeStyle` result should match a normal record update"
+
+  , ( \() ->
+      Dom.element "div"
+        |> Dom.addStyleList [ (testString1, testString2) ]
+        |> Dom.replaceStyleList [ (testString3, testString4) ]
+        |> Dom.getInternal
+        |> Expect.equal { div | styles = [ (testString3, testString4) ] }
+    )
+      |> test "`Dom.replaceStyleList` result should match a normal record update"
   ]
 
 
